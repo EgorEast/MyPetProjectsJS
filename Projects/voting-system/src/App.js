9c1593ba69af0da581e65729f060180bf34cb172
EgorEast <egoreast.true@gmail.com>
2021-04-12 12:44:55 +0300

    add rename objects and functions in App.js

--- Projects/voting-system/src/App.js
+++ Projects/voting-system/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AnsverWithVot from './Voting/AnsverWithVot';
+import OfferAndControlContainer from './Voting/OfferAndControlContainer';
 import Context from './context';
 
 function App() {
@@ -8,41 +8,98 @@ function App() {
 		notChosen: 'notChosen',
 		against: 'against',
 	};
-	let [candidateList, setCandidateList] = useState([
-		{ id: 1, vote: vote.notChosen, confirm: false, title: 'Путин' },
-		{ id: 2, vote: vote.notChosen, confirm: false, title: 'Навальный' },
+
+	let [error, setError] = useState({
+		isError: false,
+		message: '',
+	});
+
+	// let offer = {
+	// 	id: 0,
+	// 	vote: vote.notChosen,
+	// 	imgSrc: null,
+	// 	confirm: false,
+	// 	title: '',
+	// 	description: '',
+	// };
+
+	let [offersList, setOffersList] = useState([
+		{
+			id: 1,
+			vote: vote.notChosen,
+			imgSrc: null,
+			confirm: false,
+			title: 'Купить рабочую машину',
+			description: 'Машина необходима для подвоза сотрудников.',
+		},
+		{
+			id: 2,
+			vote: vote.notChosen,
+			imgSrc: null,
+			confirm: false,
+			title: 'Расширить штат тестировщиков',
+			description:
+				'С настоящей нагрузкой в офисе сотрудников-тестировщиков недостаточно для того, чтобы покрыть нужды. Предлагаю нанять в штат ещё 2-х новых сотрудников уровня junior.',
+		},
 		{
 			id: 3,
 			vote: vote.notChosen,
+			imgSrc: null,
 			confirm: false,
-			title: 'Жириновский',
+			title: 'Поднять зарплату разработчикам',
+			description: '',
 		},
 	]);
 
+	let [stateVotingSystem, setStateVotingSystem] = useState({
+		votedForAll: false,
+		onlyOneOffer: false,
+	});
+
+	let notAllConfirms = offersList.filter((offer) => !offer.confirm);
+
+	let [indexCurrOffer, setCurrIndexOffer] = React.useState(0);
+
 	function setVote(id = 0, value = '') {
-		setCandidateList(
-			candidateList.map((candidate) => {
-				if (candidate.id === id) candidate.vote = value;
-				return candidate;
+		setOffersList(
+			offersList.map((offer) => {
+				if (offer.id === id) offer.vote = value;
+				return offer;
 			})
 		);
 	}
-	function setConfirm(id = 0, value = false) {
-		setCandidateList(
-			candidateList.map((candidate) => {
-				if (candidate.id === id) candidate.confirm = value;
-				return candidate;
-			})
-		);
+	function votedForAll(status = false) {
+		setStateVotingSystem({ votedForAll: status });
 	}
 
-	let [indexCurrCandidate, setCurrIndexCandidate] = React.useState(0);
-	function setNextIndexCandidate() {
-		if (indexCurrCandidate < candidateList.length - 1)
-			setCurrIndexCandidate(indexCurrCandidate + 1);
+	function onlyOneOffer(status = false) {
+		setStateVotingSystem({ onlyOneOffer: status });
 	}
-	function setPrevIndexCandidate() {
-		if (indexCurrCandidate > 0) setCurrIndexCandidate(indexCurrCandidate - 1);
+
+	function setConfirm(id = 0, value = false) {
+		setOffersList(
+			offersList.map((offer) => {
+				if (offer.id === id) offer.confirm = value;
+				return offer;
+			})
+		);
+
+		if (notAllConfirms.length) {
+			console.log('Ещё не все выбраны');
+			votedForAll(false);
+		} else {
+			console.log('Все выбраны');
+			votedForAll(true);
+		}
+	}
+
+	function setNextIndexOffer() {
+		if (indexCurrOffer < offersList.length - 1)
+			setCurrIndexOffer(indexCurrOffer + 1);
+	}
+
+	function setPrevIndexOffer() {
+		if (indexCurrOffer > 0) setCurrIndexOffer(indexCurrOffer - 1);
 	}
 
 	return (
@@ -50,15 +107,21 @@ function App() {
 			value={{
 				setVote,
 				setConfirm,
-				setNextIndexCandidate,
-				setPrevIndexCandidate,
+				setNextIndexOffer,
+				setPrevIndexOffer,
+				votedForAll,
+				onlyOneOffer,
+				setError,
 				vote,
-				indexCurrCandidate,
+				indexCurrOffer,
+				stateVotingSystem,
+				offersList,
+				error,
 			}}
 		>
 			<div className='wrapper'>
-				<h1>Choose</h1>
-				<AnsverWithVot candidateList={candidateList} />
+				<h1>Voting</h1>
+				<OfferAndControlContainer />
 			</div>
 		</Context.Provider>
 	);
